8db0faf0fe9a2e05a25d1f8eebae51e3ecbc9d00
Gabriel-Tabajara <gabriel.tabajara@pucrs.br>
2022-10-26 10:22:37 -0300

    adding a .env file

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
-import bodyParser from 'body-parser'
-import { movieRouter } from './routes/movieRoutes.js';
+import bodyParser from "body-parser";
+import { movieRouter } from "./routes/movieRoutes.js";
+import dotenv from "dotenv";
 
 const app = express();
 
-const dbURI = 'mongodb+srv://morcego:1234@cluster0.b5g905x.mongodb.net/CRUDMovies?retryWrites=true&w=majority';
+const dbURI = process.env.MONGOURI;
 
 async function connectDb() {
-    try{
-        await mongoose.connect(dbURI);
-        app.listen(5000, () => console.log('Connected in port 5000'));
-    } catch(err) {
-        console.log('Error in connect with database');
-    }
+  try {
+    await mongoose.connect(dbURI!);
+    app.listen(5000, () => console.log("Connected in port 5000"));
+  } catch (err) {
+    console.log("Error in connect with database");
+  }
 }
 
 connectDb();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
 
-app.use('/movies', movieRouter);
+app.use("/movies", movieRouter);
 
-app.use((req: any , res: any) => {
-    res.status(404).send(JSON.stringify({ message: 'Route doesn´t exists'}));
+app.use((req: any, res: any) => {
+  res.status(404).send(JSON.stringify({ message: "Route doesn´t exists" }));
 });
